ae3e136f1f12d5e20d03eb2d7bbd24c0bff522ec
Dung Ha Viet <dunghv1@smartosc.com>
2023-10-16 18:05:20 +0700

    update-code-l2

--- SerenityTemplate/src/test/java/smartosc/SerenityFirstTest.java
+++ SerenityTemplate/src/test/java/smartosc/SerenityFirstTest.java
@@ -6,9 +6,10 @@ import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver
 
 public class SerenityFirstTest {
     public static void main(String[] args) {
-        getDriver().get("https://www.lg.com/uk/login/");
+//        getDriver().get("https://www.lg.com/uk/login/");
 
-        getDriver().findElement(By.xpath("(//a[@class='cmp-button c-button c-button--default highlight m-medium w-large only-pc'])[1]")).click();
+//        getDriver().findElement(By.xpath("(//a[@class='cmp-button c-button c-button--default highlight m-medium w-large only-pc'])[1]")).click();
+        
         System.out.println("Done");
     }
 }
